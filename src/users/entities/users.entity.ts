d974201afae8dcd6bee13407ba7fcf37d50a7f4d
JAEJIN <wowls9416@naver.com>
2024-04-04 16:34:59 +0900

    feat:[main] debug 세팅 및 user모듈 추가, post 관계설정 및 의존성 수정

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { RolesEnum } from '../const/roles.const';
+import { PostsModel } from 'src/posts/entities/posts.entity';
+
+@Entity()
+export class UsersModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 20 })
+  nickname: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Object.values(RolesEnum),
+    default: RolesEnum.USER,
+  })
+  role: RolesEnum;
+
+  @OneToMany(() => PostsModel, (post) => post.author)
+  posts: PostsModel[];
+}
