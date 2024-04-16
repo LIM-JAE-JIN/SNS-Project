a8a51b98a372b53969117702b36a31eea193ce65
JAEJIN <wowls9416@naver.com>
2024-04-16 18:42:19 +0900

    feat:[main] config모듈로 노출되면 안되는 값 env 환경변수 적용

---
+++ src/common/const/env-keys.const.ts
@@ -0,0 +1,18 @@
+// 서버 프로토콜 ex) http / https
+export const ENV_PROTOCOL_KEY = 'PROTOCOL';
+// 서버 호스트 ex) localhost:3000
+export const ENV_HOST_KEY = 'HOST';
+// JWT 토큰 시크릿 ex) jwtToken
+export const ENV_JWT_SECRET_KEY = 'JWT_SECRET';
+// JWT 토큰 해시 라운드 수 ex) 10
+export const ENV_HASH_ROUNDS_KEY = 'HASH_ROUNDS';
+// 데이터베이스 호스트 ex) localhost
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+// 데이터베이스 포트번호 ex) 5432
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+// 데이터베이스 사용자 이름 ex) postgres
+export const ENV_DB_USERNAME_KEY = 'DB_USERNAME';
+// 데이터베이스 비밀번호 ex) postgres
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+// 데이터베이스 이름 ex) postgres
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
